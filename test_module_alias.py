3dda0c1f8b1002656811ec1667c9a10fb1c79564
Tovid Dfrei <149096220+toviddfrei@users.noreply.github.com>
2024-04-14 22:04:03 +0200

    modify gitignore test_numpy env space more modules

---
+++ test_module_alias.py
@@ -0,0 +1,12 @@
+import sys
+sys.path.insert(0, './python-course/library_module3/libs')
+import helper as h
+
+def render():
+    print(h.greeting('Tiffany', 'Hudgens'))
+
+render()
+
+import math as m
+
+m.sqrt(4)
